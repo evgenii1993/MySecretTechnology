7eed8123f421e138dc423c9c7cb7eb7f0583b3b6
esaharov <esaharov@osslabs.ru>
2018-12-21 15:55:56 +0400

    new task

---
+++ c8/task/Boat.java
@@ -0,0 +1,14 @@
+public class Boat {
+	private int length;
+	public void setLength (int len) {
+		this.length = len;
+	}
+
+	public int getLength() {
+		return this.length;
+	}
+
+	public void move() {
+		System.out.print("Дрейф ");
+	}
+}
